44079dcf7fb0c448a0625b891e26b38d5b3afc67
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-07-03 15:57:23 +0200

    Add basic template

--- classroom_admin/classroom_admin.py
+++ classroom_admin/classroom_admin.py
@@ -1,9 +1,10 @@
+from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
