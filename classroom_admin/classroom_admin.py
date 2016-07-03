57a396a69542175b175540871590a0958caf92d1
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-07-03 17:52:19 +0200

    Add Google Classroom API connection

--- classroom_admin/classroom_admin.py
+++ classroom_admin/classroom_admin.py
@@ -1,12 +1,48 @@
+import json
+
+import httplib2
+import flask
+
+from apiclient import discovery
+from oauth2client import client
+
 from flask import render_template
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html')
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
 
-if __name__ == "__main__":
-    app.run()
+    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+
+    if credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        service = discovery.build('classroom', 'v1', http=http_auth)
+
+        results = service.courses().list(pageSize=10).execute()
+        courses = results.get('courses', [])
+        return render_template('index.html', courses=courses)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    flow = client.flow_from_clientsecrets(
+        'client_secret.json',
+        scope='https://www.googleapis.com/auth/classroom.courses.readonly',
+        redirect_uri=flask.url_for('oauth2callback', _external=True))
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        flask.session['credentials'] = credentials.to_json()
+        return flask.redirect(flask.url_for('index'))
+
 
 application = app
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
+import uuid
+
 from classroom_admin.classroom_admin import application
 
+application.secret_key = str(uuid.uuid4())
+application.debug = False
+
 if __name__ == "__main__":
+    import uuid
+    application.secret_key = str(uuid.uuid4())
+    application.debug = False
     application.run()
