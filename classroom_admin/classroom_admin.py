e5dccf40b818460e24a68fa528c9883440849acb
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-07-03 19:24:37 +0200

    Add test create route

--- classroom_admin/classroom_admin.py
+++ classroom_admin/classroom_admin.py
@@ -29,11 +29,32 @@ def index():
         return render_template('index.html', courses=courses)
 
 
+@app.route('/create')
+def create():
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
+
+    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+
+    if credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        service = discovery.build('classroom', 'v1', http=http_auth)
+        body = {
+            "ownerId": "killian.kemps@etu-webschoolfactory.fr",
+            "name": "second_test_course"
+        }
+
+        results = service.courses().create(body=body).execute()
+        return render_template('success.html')
+
+
 @app.route('/oauth2callback')
 def oauth2callback():
     flow = client.flow_from_clientsecrets(
         'client_secret.json',
-        scope='https://www.googleapis.com/auth/classroom.courses.readonly',
+        scope='https://www.googleapis.com/auth/classroom.courses',
         redirect_uri=flask.url_for('oauth2callback', _external=True))
     if 'code' not in flask.request.args:
         auth_uri = flow.step1_get_authorize_url()
